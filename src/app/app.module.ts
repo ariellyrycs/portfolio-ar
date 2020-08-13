3af5f7d55697c175f876b0583558da0760bbaead
Ariel <ariellyrycs@gmail.com>
2020-08-12 23:46:16 -0500

    Route modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatGridListModule
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
