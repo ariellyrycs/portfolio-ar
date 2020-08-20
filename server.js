4da32f069e8622e9d2fdb31eb0b4186076f2a92f
Ariel <ariellyrycs@gmail.com>
2020-08-19 21:57:34 -0500

    add corse library

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+const app = express();
+
+app.use(cors({
+		origin: 'http://localhost:4200'
+}));
 
 const port = 3000;
-const app = express();
  
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/content', express.static('server/personal'));
-
-app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
-	next();
-});
 
 app.get('/personal', (req, res) => {
     res.json({});
