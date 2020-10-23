a563f8e026e444940a497dcdd5d597d1152515ee
Ariel <ariellyrycs@gmail.com>
2020-10-22 22:12:27 -0500

    Add skills in home page

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 const MaterialComponents = [
   MatToolbarModule,
   MatButtonModule,
-  MatGridListModule
+  MatGridListModule,
+  MatProgressBarModule
 ];
 
 @NgModule({
